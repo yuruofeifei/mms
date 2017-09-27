9511ee923c8daeb5cb9614fd056455d979959599
kevinthesun <kevinthesunwy@gmail.com>
2017-09-27 01:10:04 -0700

    Indent fix

--- mxnet_vision_service.py
+++ mxnet_vision_service.py
@@ -3,15 +3,15 @@ from utils.mxnet_utils import Image
 
 
 class MXNetVisionService(MXNetBaseService):
-	def _preprocess(self, data):
-		img_list = []
-		for image in data:
-			img_arr = Image.read(image)
-			img_arr = Image.transform_shape(img_arr)
-			img_list.append(img_arr)
-		return img_list
+    def _preprocess(self, data):
+        img_list = []
+        for image in data:
+            img_arr = Image.read(image)
+            img_arr = Image.transform_shape(img_arr)
+            img_list.append(img_arr)
+        return img_list
 
-	def _postprocess(self, data):
-		return data
+    def _postprocess(self, data):
+        return data
 
 
