6baa0c16a066a93c0fa2fe52e231876c8ad99194
kevinthesun <kevinthesunwy@gmail.com>
2017-09-27 00:58:34 -0700

    Refactor image utility functions

--- mxnet_vision_service.py
+++ mxnet_vision_service.py
@@ -4,10 +4,12 @@ from utils.mxnet_utils import Image
 
 class MXNetVisionService(MXNetBaseService):
 	def _preprocess(self, data):
-		img_arr = []
+		img_list = []
 		for image in data:
-			img_arr.append(Image.read(image))
-		return img_arr
+			img_arr = Image.read(image)
+			img_arr = Image.transform_shape(img_arr)
+			img_list.append(img_arr)
+		return img_list
 
 	def _postprocess(self, data):
 		return data
